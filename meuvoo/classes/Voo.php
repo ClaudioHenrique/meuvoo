156a1313d7bd0ed41024e730ded64daed1cd4a0a
claudiohenrique <claudiohenriquedev@gmail.com>
2014-02-21 22:22:08 -0300

    Modificação na Classe Voo.

--- meuvoo/classes/Voo.php
+++ meuvoo/classes/Voo.php
@@ -1,12 +1,17 @@
 <?php
 class Voo{
 	
+	private $url = 'http://www.infraero.gov.br/voos/index.aspx';
+	
 	public function __construct(){
 		
 	}
 	
 	
 	private function getInformacoes($aeroporto,$numeroVoo){
-		
+		$ch = curl_init($this->url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$resultado = curl_exec($ch);
+		curl_close($ch);	
 	}
 }
